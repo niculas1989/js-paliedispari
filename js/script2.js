c2a9981acfd46e01baffa363e9f1a8c5c86a0e82
niculas1989 <nicolas.maranzano@libero.it>
2022-01-14 14:50:32 +0100

    add comments

--- js/script2.js
+++ js/script2.js
@@ -5,4 +5,4 @@ Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione)
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
 Dichiariamo chi ha vinto.
-/*
+*/
