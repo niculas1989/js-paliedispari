32031e712fa5c5175911db8ff239a57a0dd285f6
niculas1989 <nicolas.maranzano@libero.it>
2022-01-14 15:30:41 +0100

    add half solution for script1

--- js/script.js
+++ js/script.js
@@ -8,14 +8,28 @@ Creare una funzione per capire se la parola inserita è palindroma
 const userWord = document.getElementById('word');
 const buttonGenerate = document.getElementById('generate');
 const resultElement = document.getElementById('result');
+const resultContrElement = document.getElementById('result-contr');
 
+
+
+// //! Creazione funzione per "girare" la frase
+
+
+
+
+
+function reverseWord(word) {
+    let reversedWord = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversedWord += word.charAt(i);
+    }
+    return reversedWord;
+}
+
+let reWord = reverseWord(userWord.value);
 
 buttonGenerate.addEventListener('click', function () {
     resultElement.innerText = `La tua parola è: ${userWord.value}`;
+    let reWord = reverseWord(userWord.value);
+    resultContrElement.innerText = `La tua parola al contrario è: ${reWord}`;
 })
-
-
-
-//! Creazione funzione per "girare" la frase
-
-
