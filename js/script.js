80ddb365669bf16d1b763e24946eab75220cefb3
niculas1989 <nicolas.maranzano@libero.it>
2022-01-14 15:03:52 +0100

    add HTML elements + JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
+console.log('JS OK');
 /*
 Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+
+const userWord = document.getElementById('word');
+const buttonGenerate = document.getElementById('generate');
+const resultElement = document.getElementById('result');
+
+
+buttonGenerate.addEventListener('click', function () {
+    resultElement.innerText = `La tua parola è: ${userWord.value}`;
+})
+
+
 
 
